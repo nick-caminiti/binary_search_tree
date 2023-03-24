4a94c91ded590f739cd6956674e87bb094bdeb64
Nick Caminiti <nmcams11@gmail.com>
2023-03-24 08:01:23 -0700

    all methods working

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -44,13 +44,13 @@ class Tree
     current_node = @root
     until current_node.left.nil? && current_node.right.nil? 
       return if value == current_node.data
-      
+
       current_node = if value < current_node.data
-                        current_node.left
-                      else
-                        current_node.right
-                      end                    
-    
+                       current_node.left
+                     else
+                       current_node.right
+                     end
+
       if current_node.left.nil? && value < current_node.data
         current_node.left = Node.new(value) 
         return
@@ -100,7 +100,7 @@ class Tree
         replace_node_parent = replace_node
         replace_node = replace_node.left
       end
-      
+
       remove_node(replace_node, replace_node_parent)
 
       if parent_node.nil?
@@ -122,9 +122,11 @@ class Tree
       present = true if value == current_node.data
       if value < current_node.data
         break if current_node.left.nil?
-        current_node =  current_node.left   
+
+        current_node = current_node.left
       elsif value > current_node.data
         break if current_node.right.nil?
+
         current_node = current_node.right
       end
     end
@@ -140,7 +142,7 @@ class Tree
       current_node = queue[0]
       queue.shift
       queue.push(current_node.left) unless current_node.left.nil?
-      queue.push(current_node.right) unless current_node.right.nil?        
+      queue.push(current_node.right) unless current_node.right.nil?
       if block_given?
         yield current_node
       else
@@ -153,6 +155,7 @@ class Tree
   def level_order_recur(queue = [@root],return_array = [], &block)
     if queue.empty?
       return return_array unless block_given?
+
       return
     end
     current_node = queue[0]
@@ -175,7 +178,7 @@ class Tree
     end
     preorder(return_array, root.left, &block) unless root.left.nil?
     preorder(return_array, root.right, &block) unless root.right.nil?
-    return return_array
+    return_array
   end
 
   def inorder(return_array = [], root = @root, &block)
@@ -186,7 +189,7 @@ class Tree
       return_array.push(root.data)
     end
     inorder(return_array, root.right, &block) unless root.right.nil?
-    return return_array
+    return_array
   end
 
   def postorder(return_array = [], root = @root, &block)
@@ -197,35 +200,114 @@ class Tree
     else
       return_array.push(root.data)
     end
-    return return_array
+    return_array
   end
 
+  def height(node, height = 0)
+    return height if node.left.nil? && node.right.nil?
+
+    height += 1
+
+    left_height = if node.left.nil?
+                    0
+                  else
+                    height(node.left, height)
+                  end
+
+    right_height = if node.right.nil?
+                     0
+                   else
+                     height(node.right, height)
+                   end
+    left_height > right_height ? left_height : right_height
+  end
+
+  def depth(node)
+    return nil if node.nil?
+
+    current_node = @root
+    depth = 0
+    until current_node == node || (current_node.left.nil? && current_node.right.nil?)
+      depth += 1
+      current_node = node.data < current_node.data ? current_node.left : current_node.right
+    end
+    depth
+  end
+
+  def balanced?
+    (height(@root.left) - height(@root.right)).abs <= 1
+  end
+
+  def rebalance
+    new_array = []
+    self.inorder do |node|
+      new_array << node.data
+    end
+    @root = build_tree(new_array)
+  end
 end
 
-array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-# [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]
-p array.uniq.sort
-tree = Tree.new(array)
-p tree
-tree.pretty_print
-tree.insert(2)
-tree.insert(320)
-tree.insert(323)
-tree.insert(321)
-tree.insert(320)
-tree.insert(319)
-tree.insert(315)
-tree.insert(24)
+# array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+# # [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]
+# p array.uniq.sort
+# tree = Tree.new(array)
+# # p tree
 # tree.pretty_print
-tree.insert(9)
-tree.insert(30)
-# tree.insert()
-tree.insert(6)
-tree.pretty_print
-tree.delete(324)
-tree.pretty_print
-puts tree.find(67)
-# tree.level_order_recur { |node| puts node.data }
-# p tree.level_order_recur
-tree.postorder { |node| puts node.data }
-p tree.postorder
+# tree.insert(2)
+# tree.insert(320)
+# tree.insert(323)
+# tree.insert(321)
+# tree.insert(320)
+# tree.insert(319)
+# # tree.insert(319)
+# tree.insert(315)
+# tree.insert(316)
+# tree.insert(24)
+# # tree.pretty_print
+# tree.insert(9)
+# tree.insert(30)
+# # tree.insert()
+# tree.insert(6)
+# tree.pretty_print
+# tree.delete(324)
+# tree.pretty_print
+# # puts tree.find(67)
+# # tree.level_order_recur { |node| puts node.data }
+# # p tree.level_order_recur
+# # tree.preorder { |node| puts node.data }
+# # p tree.preorder
+# # p tree.height(tree.find(320))
+# # p tree.depth(tree.find(6))
+# p tree.balanced?
+# tree.rebalance
+# tree.pretty_print
+# p tree.balanced?
+
+test_array = (Array.new(15) {rand(1..100) })
+test_tree = Tree.new(test_array)
+test_tree.pretty_print
+puts test_tree.balanced?
+
+# test_tree.level_order { |node| puts node.data }
+# test_tree.preorder { |node| puts node.data }
+# test_tree.inorder { |node| puts node.data }
+# test_tree.postorder { |node| puts node.data }
+
+test_tree.insert(101)
+test_tree.insert(102)
+test_tree.insert(103)
+test_tree.insert(104)
+test_tree.insert(105)
+
+test_tree.pretty_print
+puts test_tree.balanced?
+
+test_tree.rebalance
+
+test_tree.pretty_print
+puts test_tree.balanced?
+
+# test_tree.level_order { |node| puts node.data }
+# test_tree.preorder { |node| puts node.data }
+test_tree.inorder { |node| puts node.data }
+# test_tree.postorder { |node| puts node.data }
